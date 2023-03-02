5bfdec0ceb92ef349eb5f09e655d513781f3c6e6
krunal11111 <112586703+krunal11111@users.noreply.github.com>
2023-03-01 19:48:32 -0500

    i have made changes as required. branch name is an interview.

--- src/test/java/fun/GameTests.java
+++ src/test/java/fun/GameTests.java
@@ -2,5 +2,13 @@ package fun;
 
 public class GameTests {
 
+    public static void main(String[] args) {
+        System.out.println(" Hello , this is my jar ");
+        if (args.length > 0 ){
+            System.out.println(" i have some changes to make ");
+            for(String arg : args) System.out.println("one of my args: "+ arg);
+        }
+    }
+
 
 }
